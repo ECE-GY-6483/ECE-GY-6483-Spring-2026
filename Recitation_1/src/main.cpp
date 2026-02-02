7981b69b5fa34ca653fd57e65af17b1be02bcf72
Jotheesh Kummathi <66893484+jotheesh1729@users.noreply.github.com>
2026-02-02 14:23:27 -0500

    recitation1

--- Recitation_1/src/main.cpp
+++ Recitation_1/src/main.cpp
@@ -1,14 +1,16 @@
 #include <mbed.h>
 
-DigitalOut led(LED1);
+DigitalOut led(LED2);
 
 int main(){
+    while(1){
 
-    led = 1; // Turn the LED on
-    thread_sleep_for(500); // Wait for 500 milliseconds
-    //wait_us(500000); // Alternative wait function
+        led = 1; // Turn the LED on
+        thread_sleep_for(500); // Wait for 500 milliseconds
+        //wait_us(500000); // Alternative wait function
 
-    led = 0; // Turn the LED off
-    thread_sleep_for(500); // Wait for 500 milliseconds
-    //wait_us(500000); // Alternative wait function
+        led = 0; // Turn the LED off
+        thread_sleep_for(500); // Wait for 500 milliseconds
+        //wait_us(500000); // Alternative wait function
+    }
 }
